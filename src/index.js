535a44892259687e701cd95601c2a5b8af77a110
parthj39 <parth010872@gmail.com>
2021-10-15 19:22:51 +0530

    security fix

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './containers/App';
 import reportWebVitals from './reportWebVitals';
-import Particles from 'react-particles-js';
 import 'tachyons';
 ReactDOM.render(
   <React.StrictMode>
